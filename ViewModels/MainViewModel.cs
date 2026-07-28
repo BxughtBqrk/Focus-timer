13e4e6a1b42de633c698ab5016b9f5f8a20ba5a5
Developer <developer@example.com>
2026-07-28 19:17:42 +0530

    Refactor: Remove unlockable themes

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -178,7 +178,6 @@ public partial class MainViewModel : ViewModelBase
         public bool IsAlwaysOnTop { get; set; } = false;
         public double BackgroundOpacity { get; set; } = 0.88;
         public double GhostOpacity { get; set; } = 1.0;
-        public string SelectedTheme { get; set; } = "Cyber";
     }
 
     public class HistoryRecord
@@ -193,7 +192,6 @@ public partial class MainViewModel : ViewModelBase
         public List<HistoryRecord> Records { get; set; } = new List<HistoryRecord>();
         public int CurrentStreak { get; set; } = 0;
         public string LastFocusedDate { get; set; } = "";
-        public List<string> UnlockedThemes { get; set; } = new List<string> { "Cyber" };
     }
 
     private HistoryData _history = new HistoryData();
@@ -216,30 +214,10 @@ public partial class MainViewModel : ViewModelBase
                     IsAlwaysOnTop = s.IsAlwaysOnTop;
                     BackgroundOpacity = s.BackgroundOpacity;
                     GhostOpacity = s.GhostOpacity;
-                    _selectedThemeStr = s.SelectedTheme; // private field for current theme
                 }
             }
         }
         catch { }
-    }
-
-    private string _selectedThemeStr = "Cyber";
-
-    public List<string> AvailableThemes => _history.UnlockedThemes;
-
-    public string SelectedThemeOption
-    {
-        get => _selectedThemeStr;
-        set
-        {
-            if (_history.UnlockedThemes.Contains(value))
-            {
-                _selectedThemeStr = value;
-                OnPropertyChanged(nameof(SelectedThemeOption));
-                UpdateThemeBrush();
-                SaveSettings();
-            }
-        }
     }
 
     private void SaveSettings()
@@ -255,8 +233,7 @@ public partial class MainViewModel : ViewModelBase
                 IsGhostMode = this.IsGhostModeEnabled,
                 IsAlwaysOnTop = this.IsAlwaysOnTop,
                 BackgroundOpacity = this.BackgroundOpacity,
-                GhostOpacity = this.GhostOpacity,
-                SelectedTheme = this._selectedThemeStr
+                GhostOpacity = this.GhostOpacity
             };
             File.WriteAllText("settings.json", JsonSerializer.Serialize(s));
         }
@@ -302,53 +279,6 @@ public partial class MainViewModel : ViewModelBase
             _history.CurrentStreak = 0;
         }
         CurrentStreak = _history.CurrentStreak;
-        CheckThemeUnlocks();
-    }
-
-    private void CheckThemeUnlocks()
-    {
-        bool changed = false;
-        if (!_history.UnlockedThemes.Contains("Cyber")) { _history.UnlockedThemes.Add("Cyber"); changed = true; }
-        if (CurrentStreak >= 3 && !_history.UnlockedThemes.Contains("Lava"))
-        {
-            _history.UnlockedThemes.Add("Lava");
-            changed = true;
-        }
-        if (CurrentStreak >= 7 && !_history.UnlockedThemes.Contains("Matrix"))
-        {
-            _history.UnlockedThemes.Add("Matrix");
-            changed = true;
-        }
-        if (changed)
-        {
-            OnPropertyChanged(nameof(AvailableThemes));
-            SaveHistory();
-        }
-        UpdateThemeBrush();
-    }
-
-    private void UpdateThemeBrush()
-    {
-        Color start = Color.Parse("#00E5FF");
-        Color end = Color.Parse("#9D00FF");
-
-        if (_selectedThemeStr == "Lava")
-        {
-            start = Color.Parse("#FF3300");
-            end = Color.Parse("#FF8800");
-        }
-        else if (_selectedThemeStr == "Matrix")
-        {
-            start = Color.Parse("#00FF00");
-            end = Color.Parse("#003300");
-        }
-
-        var stops = new GradientStops
-        {
-            new GradientStop(start, 0),
-            new GradientStop(end, 1)
-        };
-        ThemeGradientBrush = new LinearGradientBrush { StartPoint = new RelativePoint(0, 0, RelativeUnit.Relative), EndPoint = new RelativePoint(1, 1, RelativeUnit.Relative), GradientStops = stops };
     }
 
     private void UpdateStatsDisplay()
@@ -411,6 +341,13 @@ public partial class MainViewModel : ViewModelBase
 
     public MainViewModel()
     {
+        var stops = new GradientStops
+        {
+            new GradientStop(Color.Parse("#00E5FF"), 0),
+            new GradientStop(Color.Parse("#9D00FF"), 1)
+        };
+        ThemeGradientBrush = new LinearGradientBrush { StartPoint = new RelativePoint(0, 0, RelativeUnit.Relative), EndPoint = new RelativePoint(1, 1, RelativeUnit.Relative), GradientStops = stops };
+
         LoadSettings();
         LoadHistory();
         _timer = new DispatcherTimer
